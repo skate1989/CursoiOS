018963b70dd202354b9ada34bf1ad7b09be37c65
Jose Enrique <josemh941@gmail.com>
2026-08-06 22:07:31 -0600

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Jose Enrique Martinez Hernandez on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
