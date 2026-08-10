7afb591cc1078b82eaacab174e7af44ae5ee716e
Jose Enrique <josemh941@gmail.com>
2026-08-09 22:15:56 -0600

    Se crea el ejemplo 2

---
+++ CursoiOS/Components/Example2.swift
@@ -0,0 +1,18 @@
+//
+//  Example2.swift
+//  CursoiOS
+//
+//  Created by Jose Enrique Martinez Hernandez on 09/08/26.
+//
+
+import SwiftUI
+
+struct Example2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Example2()
+}
